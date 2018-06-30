47ead44ca70b93f3e999bddc2a9c36c874a85dbb
Figo96IT <figo96it@gmail.com>
2018-06-30 12:43:55 +0200

    Final forms

--- school-client/src/main/java/pl/sda/school/controller/SchoolClassController.java
+++ school-client/src/main/java/pl/sda/school/controller/SchoolClassController.java
@@ -3,7 +3,12 @@ package pl.sda.school.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.sda.model.Classroom;
+import pl.sda.model.Student;
 import pl.sda.school.iservice.IClassService;
 
 @Controller
@@ -20,5 +25,18 @@ public class SchoolClassController {
     public String listStudentsGradesView(Model model){
         model.addAttribute("classes", classService.findAll());
         return "classesList";
+    }
+
+    @PostMapping("classroom/add")
+    public String addClassroom(@ModelAttribute Classroom classroom) {
+        classService.save(classroom);
+        return "redirect:/";
+    }
+
+    @GetMapping("classroom/add")
+    public String addClassroomView(Model model) {
+        //todo uzyj studentservic
+        model.addAttribute("classroom", new Classroom());
+        return "addClass";
     }
 }
--- school-client/src/main/java/pl/sda/school/iservice/IClassService.java
+++ school-client/src/main/java/pl/sda/school/iservice/IClassService.java
@@ -4,6 +4,7 @@ package pl.sda.school.iservice;
 import pl.sda.model.Classroom;
 import pl.sda.model.Employee;
 import pl.sda.model.School;
+import pl.sda.model.Student;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,5 +15,9 @@ public interface IClassService {
         List<Classroom> classes = new ArrayList<>();
         classes.add(clas);
         return classes;
+    }
+
+    default void save(Classroom Classroom){
+        //tdo
     }
 }
