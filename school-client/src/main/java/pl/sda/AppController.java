f04e6c8d3f7e13dde15ce98de649220440fcab2c
Figo96IT <figo96it@gmail.com>
2018-06-30 23:05:20 +0200

    beginning slf4j

--- school-client/src/main/java/pl/sda/AppController.java
+++ school-client/src/main/java/pl/sda/AppController.java
@@ -1,5 +1,7 @@
 package pl.sda;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +11,14 @@ import java.util.Map;
 @Controller
 public class AppController {
 
+    private static final Logger logger = LoggerFactory.getLogger(AppController.class);
+
     @Value("${welcome.message:test}")
     private String message = "Hello World";
 
     @RequestMapping("/")
     public String welcome(Map<String, Object> model) {
+        logger.debug("Welcome {}", "test");
         model.put("message", this.message);
         return "welcome";
     }
